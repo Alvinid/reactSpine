ef683021658795d20c1c5d2120a862af6049bb8d
qi.zhang <qi.zhang1@connext.com.cn>
2018-08-23 15:10:35 +0800

    zq

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const app = express();
+
+app.get('/',function(req,res){
+    console.log(req.body,'123')
+    res.send('hello world')
+})
+const server=app.listen('8800',()=>{
+    const host=server.address().address;
+    const port=server.address();
+    console.log('localhost:8800'+host,port)
+})
