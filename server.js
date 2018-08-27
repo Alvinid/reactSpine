0d2f7fc84d79d28d8bce9318b67e15921e6909bd
qi.zhang <qi.zhang1@connext.com.cn>
2018-08-27 17:54:51 +0800

    server

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require ('express');
 const app = express();
-
+app.use(express.static('public'));
 app.get('/',function(req,res){
     console.log(req.body,'123')
-    res.send('hello world')；
+    res.send(`<html >
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <script src="https://cdn.bootcss.com/react/16.4.0/umd/react.development.js"></script>
+        <script src="https://cdn.bootcss.com/react-dom/16.4.0/umd/react-dom.development.js"></script>
+        <script src="https://cdn.bootcss.com/babel-standalone/6.26.0/babel.min.js"></script>
+    </head>
+    <body>
+        <div id="exp">111</div>
+        https://cnodejs.org/topic/5865a866189fd5ad6459006c
+    </body>
+    </html>`)
 })
+
 const server=app.listen('8800',()=>{
     const host=server.address().address;
     const port=server.address();
